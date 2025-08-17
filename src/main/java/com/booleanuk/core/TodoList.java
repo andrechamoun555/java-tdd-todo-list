c9701a6f5e28283b041129f2977f6a00a83a8b00
andrechamoun555 <andre.chamoun03@gmail.com>
2025-08-17 18:52:18 +0200

    Test and domain model are done

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,50 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TodoList {
+
+
+
+    public Boolean addTask(String name) {
+        return null;
+    }
+
+    public ArrayList<String> getTasks() {
+        return null;
+    }
+
+
+
+    public Boolean changeStatus(String name) {
+        return null;
+    }
+
+    public ArrayList<String> getCompletedTasks() {
+        return null;
+    }
+
+    public ArrayList<String> getIncompleteTasks() {
+        return null;
+    }
+
+    public String search(String name){
+        return null;
+    }
+
+    public boolean deleteTask(String name){
+        return false;
+    }
+
+    public ArrayList<String> sortTasksAscOrder(){
+        return null;
+    }
+
+    public ArrayList<String> sortTasksDescOrder(){
+        return null;
+    }
+
+
 
 }
